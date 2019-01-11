a5f07b15db49013ef609715c6fb28975e764d384
Waylon S Walker <quadmx08@gmail.com>
2019-01-10 19:27:03 -0600

    changed gatsby node to prevent pages without templates being added to create pages.

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,17 +40,20 @@ exports.createPages = ({ actions, graphql }) => {
 
         posts.forEach(edge => {
             const id = edge.node.id
-            createPage({
-                path: edge.node.fields.slug,
-                tags: edge.node.frontmatter.tags,
-                component: path.resolve(
-                    `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-                ),
-                // additional data can be passed via context
-                context: {
-                    id,
-                },
-            })
+            if (edge.node.frontmatter.templateKey) {
+
+                createPage({
+                    path: edge.node.fields.slug,
+                    tags: edge.node.frontmatter.tags,
+                    component: path.resolve(
+                        `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+                    ),
+                    // additional data can be passed via context
+                    context: {
+                        id,
+                    },
+                })
+            }
         })
 
         // Tag pages:
