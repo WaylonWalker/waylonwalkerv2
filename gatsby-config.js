eaf3d6af1fb1821a4242d122bbefcb25cb6911dd
Waylon Walker <walker_waylon@cat.com>
2019-01-13 08:56:16 -0600

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+// google analytics tracking id
+// UA-113148616-1
+
 module.exports = {
   siteMetadata: {
     title: 'Waylon Walker',
@@ -48,6 +51,13 @@ module.exports = {
         icon: 'src/images/W.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-113148616-1",
+        exclude: ['/admin/**'],
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
