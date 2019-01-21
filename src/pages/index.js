143d000de97011976a63bafa57537df666a3aedc
Waylon Walker <walker_waylon@cat.com>
2019-01-21 11:32:57 -0600

    WIP About

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,15 @@ export default class IndexPage extends React.Component {
         <div id="content" style={{ maxWidth: '1200px', margin: 'auto' }}>
           <section>
             <h1>About</h1>
+            <p style={{maxWidth: '800px', margin: '2rem auto'}}>
+              I am a <strong>Husband</strong> and <strong>Father</strong> of 2 energetic Children.  They are the ones that get me out of bed in the morning.
+              Literally, no one beats <em>Wyatt</em> out of bed.  He is our crazy, never slowing down son, who his filled with <strong>Love</strong> and <strong>Compassion</strong>.  When someone is having a bad day, he is the first to notice and try to help in every way he can.  He is always trying to help <em>Ayla</em> throughout the day.  
+           </p>
+
             <Image />
+            <p style={{padding: '2rem', margin: '2rem'}}>
+
+            </p>
           </section>
           <section>
             <h1>Blog</h1>
