ba0c57f87a1707c221fcb186b56727dfe363524b
WaylonWalker <walker_waylon_s@cat.com>
2020-01-20 10:34:42 -0600

    added feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Waylon Walker',
-    siteUrl: 'https://waylonwalker.com'
+    siteUrl: 'https://waylonwalker.com',
+    description: "Waylon Walker's personal website"
   },
   plugins: [
     // `gatsby-plugin-sass`,
@@ -87,5 +88,65 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter === 'blog-post' ? '/blog/' : edge.node.frontmatter === 'notes' ? '/notes/' : '' + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter === 'blog-post' ? '/blog/' : edge.node.frontmatter === 'notes' ? '/notes/' : '' + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Waylon Walker's Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/blog/",
+            // optional configuration to specify external rss feed, such as feedburner
+            link: "https://feeds.feedburner.com/gatsby/blog",
+          },
+        ],
+      },
+    },
   ],
 }
