0fd28e79f3fb54a954d63ba0bfafdfb05771179f
WaylonWalker <walker_waylon_s@cat.com>
2020-05-21 10:10:20 -0500

    FIX Twitter Image

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -10,6 +10,7 @@ export default function TestPage() {
                 meta={[
                     { name: 'og:title', content: "Waylon Walker's Newsletter" },
                     { name: 'og:image', content: 'https://waylonwalker.com/waylon-walker-newsletter.png' },
+                    { name: 'twitter:image', content: 'https://waylonwalker.com/waylon-walker-newsletter.png' },
                     { name: 'twitter:title', content: "Waylon Walker's Newsletter" },
                 ]}
             />
