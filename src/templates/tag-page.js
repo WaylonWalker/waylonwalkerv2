33cbe63424c65072d4c7ff1a28c98b3bf5b41382
Waylon Walker <quadmx08@gmail.com>
2020-12-27 10:56:44 -0600

    implement tag page

---
+++ src/templates/tag-page.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Layout from '../components/layout'
+import BlogPosts from '../components/BlogPosts'
+
+const TagPage = ({ data, pageContext }) => {
+
+  return (
+    <Layout
+      description = {`Waylon Walker's ${pageContext.tag} posts`}
+      title = {`pageContext.tag} posts`}
+      keywords = { pageContext.tag }
+      url = {`https://waylonwalker.com/${pageContext.tag}`}
+      // time={post.frontmatter.date}
+    >
+      <h1>{pageContext.tag}</h1>
+      <BlogPosts posts={pageContext.posts} />
+    </Layout>
+  )
+}
+
+export default TagPage
+
